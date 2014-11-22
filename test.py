d6ba2d502f0e9709072ffaa475c3e4056e3435d9
Gia Sadhwani <giamail@gmail.com>
2014-11-22 11:39:26 +0000

    Initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+'''
+Created on 22 Nov 2014
+
+@author: Gia
+'''
+
+def test():
+    print("test")
+
+
+def main():
+    print("Hello World")
+    print("and again")
+
+if __name__ == "__main__":
+    main()
